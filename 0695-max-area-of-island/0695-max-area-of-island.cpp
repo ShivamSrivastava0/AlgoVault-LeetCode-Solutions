279cbe1c357aec8a9f6b6b43b31299d8bb980403
Shivam Srivastava <shivamsrivastava5095@gmail.com>
2026-07-10 18:53:05 +0530

    Time: 48 ms (5.13%), Space: 39.6 MB (5.19%) - LeetHub

---
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int bfs(vector<vector<int>>& grid, int i, int j, set<pair<int, int>>& vis, vector<vector<int>>& directions, int m, int n){
+        queue<pair<int,int>> q;
+        vis.insert({i,j});
+        q.push({i,j});
+        
+        int area=0;
+        while(!q.empty()){
+            pair<int, int> p = q.front();
+            q.pop();
+            
+            area++;
+
+            int s1 = p.first;
+            int s2 = p.second;
+            for(auto dir : directions){
+                int new_i = dir[0] +s1;
+                int new_j = dir[1] +s2;
+
+                if(new_i>=0 && new_i<m && new_j>=0 && new_j<n && grid[new_i][new_j]==1 && vis.find({new_i,new_j})==vis.end()){
+                    q.push({new_i,new_j});
+                    vis.insert({new_i,new_j});
+                }
+            }
+        }
+    return area;
+    }
+
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        int mx = 0;
+
+        set<pair<int, int>> vis;
+        vector<vector<int>> directions = {{0,1},{0,-1},{1,0},{-1,0}};
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==1 && vis.find({i,j})==vis.end()){
+                    mx = max(mx, bfs(grid, i, j, vis, directions, m,n));  
+                }
+            }
+        }
+    return mx;
+    }
+};
