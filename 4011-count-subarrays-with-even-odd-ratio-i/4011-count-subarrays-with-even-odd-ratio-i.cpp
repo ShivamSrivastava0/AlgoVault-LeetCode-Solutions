0308fb0c232dfb23fc95c04c57adc8c04e61ba79
Shivam Srivastava <shivamsrivastava5095@gmail.com>
2026-08-05 16:46:20 +0530

    Time: 51 ms (21.07%), Space: 29.8 MB (54.13%) - LeetHub

---
+++ 4011-count-subarrays-with-even-odd-ratio-i/4011-count-subarrays-with-even-odd-ratio-i.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countRatioSubarrays(vector<int>& nums, int a, int b) {
+        int c=0;
+        for(int i=0;i<nums.size();i++){
+            int even=0,odd=0;
+            for(int j=i;j<nums.size();j++){
+                if(nums[j]%2==0) even++;
+                else odd++;
+                if(even*b<=odd*a){c++;}
+            }
+        }
+    return c;
+    }
+};
